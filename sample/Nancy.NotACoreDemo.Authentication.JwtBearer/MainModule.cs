1b2ed0ca5851494ad8d10190308239a659fdc5b6
catcherwong <catcher_hwq@outlook.com>
2017-09-13 21:30:24 +0800

    add support for .net 4.5

---
+++ sample/Nancy.NotACoreDemo.Authentication.JwtBearer/MainModule.cs
@@ -0,0 +1,27 @@
+﻿namespace Nancy.NotACoreDemo.Authentication.JwtBearer
+{
+    using Nancy.Security;
+    using System.Text;
+
+    public class MainModule : Nancy.NancyModule
+    {
+        public MainModule()
+        {
+            this.RequiresAuthentication();
+
+            Get("/", _ =>
+            {
+                StringBuilder sb = new StringBuilder(1024);
+
+                foreach (var item in this.Context.CurrentUser.Claims)
+                {
+                    sb.Append(item.Type + ":" + item.Value);
+                    sb.AppendLine();
+                }
+
+                return sb.ToString();
+            });
+        }
+
+    }
+}
---
+++ sample/Nancy.NotACoreDemo.Authentication.JwtBearer/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Nancy.NotACoreDemo.Authentication.JwtBearer
+{
+    using Nancy.Hosting.Self;
+    using System;
+    using System.Diagnostics;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var nancyHost = new NancyHost(new Uri("http://localhost:8888/nancy/")))
+            {
+                nancyHost.Start();
+
+                Console.WriteLine("Nancy now listening - navigating to http://localhost:8888/nancy/. Press enter to stop");
+                try
+                {
+                    Process.Start("http://localhost:8888/nancy/");
+                }
+                catch (Exception)
+                {
+                }
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Stopped. Good bye!");
+        }
+    }
+}
